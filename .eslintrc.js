383971abd6eb153b91f49013e58b170a7526a5ba
Fang Peijie <17620138083@163.com>
2021-05-27 15:14:59 +0800

    添加sass支持和Eslint校验。

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,25 @@
 /*
- * @Descripttion: 
+ * @Descripttion:
  * @Author: Fang Peijie
  * @Date: 2021-05-20 11:08:37
  * @LastEditors: Fang Peijie
- * @LastEditTime: 2021-05-21 17:55:09
+ * @LastEditTime: 2021-05-27 15:06:33
  */
 module.exports = {
-  
-}
+  extends: "eslint:recommended",
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "babel-eslint",
+    ecmaVersion: 6,
+    sourceType: "module", //指定源代码存在的位置，script | module，默认为script
+  },
+  globals: {
+    document: true,
+    window: true,
+  },
+  rules: {
+    semi: 0, // 关闭分号结尾
+  },
+};
+
+// 参考：https://segmentfault.com/a/1190000024509889
